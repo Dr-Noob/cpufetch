a7e34a14908d9d09a8268e9245d19959962163d3
Dr-Noob <peibolms@gmail.com>
2021-08-08 21:12:16 +0200

    [v0.99] Add dummy unknown logo

--- src/common/ascii.h
+++ src/common/ascii.h
@@ -158,39 +158,18 @@ $C1######### ################## ########   ####   ########  \
 $C1                                                         \
 $C1######### ################   ########    ##    ########  "
 
-#define ASCII_UNKNOWN \
-"                                                              \
-                                                              \
-                                                              \
-                                                              \
-                                                              \
-                                                              \
-                                                              \
-                                                              \
-                                                              \
-                                                              \
-                                                              \
-                                                              \
-                                                              \
-                                                              \
-                                                              \
-                                                              \
-                                                              \
-                                                              \
-                                                              "
-
 //                                         LOGO            W   H   COLORS LOGO (>0 && <10)     COLORS TEXT (=2)
 //                                         --------------------------------------------------------------------------------
 static struct ascii_logo logo_amd      = { ASCII_AMD,      39, 15, {COLOR_WHITE, COLOR_GREEN}, {COLOR_WHITE, COLOR_GREEN}  };
 static struct ascii_logo logo_intel    = { ASCII_INTEL,    62, 19, {COLOR_CYAN},               {COLOR_CYAN,  COLOR_WHITE}  };
 static struct ascii_logo logo_snapd    = { ASCII_SNAPD,    39, 16, {COLOR_RED, COLOR_WHITE},   {COLOR_RED,   COLOR_WHITE}  };
 static struct ascii_logo logo_mtk      = { ASCII_MTK,      59,  5, {COLOR_BLUE, COLOR_YELLOW}, {COLOR_BLUE,  COLOR_YELLOW} };
-static struct ascii_logo logo_exynos   = { ASCII_EXYNOS,   22, 13, {COLOR_BLUE, COLOR_WHITE},  {COLOR_BLUE,  COLOR_WHITE}, };
+static struct ascii_logo logo_exynos   = { ASCII_EXYNOS,   22, 13, {COLOR_BLUE, COLOR_WHITE},  {COLOR_BLUE,  COLOR_WHITE}  };
 static struct ascii_logo logo_kirin    = { ASCII_KIRIN,    53, 12, {COLOR_RED},                {COLOR_WHITE, COLOR_RED}    };
 static struct ascii_logo logo_broadcom = { ASCII_BROADCOM, 44, 19, {COLOR_WHITE, COLOR_RED},   {COLOR_WHITE, COLOR_RED}    };
 static struct ascii_logo logo_arm      = { ASCII_ARM,      42,  5, {COLOR_CYAN},               {COLOR_WHITE, COLOR_CYAN}   };
-static struct ascii_logo logo_ibm      = { ASCII_IBM,      57, 14, {COLOR_CYAN, COLOR_WHITE},  {COLOR_CYAN,  COLOR_WHITE}, };
-static struct ascii_logo logo_unknown  = { ASCII_UNKNOWN,  62, 19, {COLOR_BLUE},               {COLOR_BLUE}                };
+static struct ascii_logo logo_ibm      = { ASCII_IBM,      57, 14, {COLOR_CYAN, COLOR_WHITE},  {COLOR_CYAN,  COLOR_WHITE}  };
+static struct ascii_logo logo_unknown  = { NULL,            0,  0, { },                        {"",          ""}           };
 
 static struct ascii_logo* ASCII_ARRAY [] = {
   &logo_amd,
