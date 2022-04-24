0f1c2881d9976e87dca983b1809d467f9af9b562
Dr-Noob <peibolms@gmail.com>
2022-04-24 11:42:32 +0100

    [v1.02] Bump version

--- src/common/main.c
+++ src/common/main.c
@@ -33,7 +33,7 @@
   static const char* OS_STR = "Unknown OS";
 #endif
 
-static const char* VERSION = "1.01";
+static const char* VERSION = "1.02";
 
 void print_help(char *argv[]) {
   const char **t = args_str;
