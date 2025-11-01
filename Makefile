Dr-Noob/cpufetch

commit 65378aaed935d40dbc9b28f3b18125d2220e06c7

[v1.05] Move sysctl from ARM-specific to common (#251)

Makefile
@@ -43,8 +43,8 @@ ifneq ($(OS),Windows_NT)
 		CFLAGS += -DARCH_ARM -Wno-unused-parameter -std=c99 -fstack-protector-all
 
 		ifeq ($(os), Darwin)
-			SOURCE += $(SRC_DIR)sysctl.c
-			HEADERS += $(SRC_DIR)sysctl.h
+			SOURCE += $(COMMON_SRC)sysctl.c
+			HEADERS += $(COMMON_SRC)sysctl.h
 		endif
         else ifeq ($(arch), $(filter $(arch), riscv64 riscv32))
                 SRC_DIR=src/riscv/

src/arm/midr.c
@@ -10,7 +10,7 @@
   #include <asm/hwcap.h>
   #include "../common/freq.h"
 #elif defined __APPLE__ || __MACH__
-  #include "sysctl.h"
+  #include "../common/sysctl.h"
 #endif
 
 #include "../common/global.h"

src/arm/soc.c
@@ -11,7 +11,7 @@
 #include "../common/pci.h"
 
 #if defined(__APPLE__) || defined(__MACH__)
-  #include "sysctl.h"
+  #include "../common/sysctl.h"
 #endif
 
 #define NA -1

src/common/sysctl.c
@@ -4,8 +4,8 @@
 #include <string.h>
 #include <errno.h>
 
-#include "../common/global.h"
-#include "../common/cpu.h"
+#include "global.h"
+#include "cpu.h"
 
 uint32_t get_sys_info_by_name(char* name) {
   size_t size = 0;

commit b1f20cfec6ff902975919ab6f3fd18be744f16fe

[v1.07] Fix bug introduced by #251 (reported in #375)
PR #251 added:

```
if defined (__FreeBSD__) || defined (__APPLE__)
...
get_sys_info_by_name
```

that function is available in src/common/sysctl.c.
In the Makefile it builds with sysctl.c but only under FreeBSD:

```
ifeq ($(os), FreeBSD)
```

So it fails under macOS as reported by #375

Makefile
@@ -30,7 +30,7 @@ ifneq ($(OS),Windows_NT)
 			HEADERS += $(SRC_DIR)freq/freq.h
 			CFLAGS += -pthread
 		endif
-		ifeq ($(os), FreeBSD)
+		ifeq ($(os), $(filter $(os), FreeBSD Darwin))
 			SOURCE += $(SRC_COMMON)sysctl.c
 			HEADERS += $(SRC_COMMON)sysctl.h
 		endif